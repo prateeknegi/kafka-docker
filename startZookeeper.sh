584861473ef83b76157b22dc75ab144c277c27c7
Prateek Negi <prateeknegi@Muktas-MacBook-Pro.local>
2018-04-21 15:56:14 -0400

    again

--- startZookeeper.sh
+++ startZookeeper.sh
@@ -6,8 +6,8 @@ dataDir="/tmp/zookeeper"
 zkConfDir="/kafka/config"
 
 #TODO
-#dataDir="/Users/prateeknegi/kafka-docker/zoo-data"
-#zkConfDir="/Users/prateeknegi/kafka-docker/zk_config"
+dataDir="/Users/prateeknegi/kafka-docker/zoo-data"
+zkConfDir="/Users/prateeknegi/kafka-docker/zk_config"
 
 echo "$zk_id" | tee $dataDir/myid
 
@@ -18,6 +18,7 @@ then
 fi
 
 IFS=', ' read -r -a zk_nodes <<< "$zk_nodes_in"
+zk_config="initLimit=5"$'\n'"syncLimit=2"$'\n'"tickTime=2000"$'\n'
 
 for index in "${!zk_nodes[@]}"
 do
@@ -28,7 +29,6 @@ do
         # if IP's are used instead of hostnames, every ZooKeeper host has to specify itself as follows
         zk_node=0.0.0.0
     fi
-  zk_config="initLimit=5\nsyncLimit=2\ntickTime=2000\n"
   zk_config="$zk_config"$'\n'"server.$zk_node_id=$zk_node:2888:3888"
 done
 
